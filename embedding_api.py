010369ba415d6698f2378e401d9089a3be9775b6
centonhuang <centonhuang@tencent.com>
2024-03-27 23:37:54 +0800

    ✨ feat(mas): support logger middleware

--- embedding_api.py
+++ embedding_api.py
@@ -1,8 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.config.env import PORT
 from src.api.router import root_router, v1_router
+from src.config.env import PORT
+from src.middleware.logger import LoggerMiddleWare
 
 
 def main():
@@ -14,10 +15,13 @@ def main():
     app.include_router(root_router)
     app.include_router(v1_router)
 
+    app.add_middleware(LoggerMiddleWare)
+
     uvicorn.run(
         app,
         host="0.0.0.0",
         port=PORT,
+        log_level="critical",  # ban uvicorn logger
     )
 
 
