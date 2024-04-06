bffa81e8b71bd2a9d9be48c2e5f80f71f0d32464
centonhuang <centonhuang@tencent.com>
2024-04-06 16:28:26 +0800

    ✨ feat(mas): using st instead of pytorch inference

--- src/api/router/v1/reranker.py
+++ src/api/router/v1/reranker.py
@@ -1,40 +1,37 @@
 from pathlib import Path
 from typing import Dict, List
 
-import torch
-import torch.nn.functional as F
 from fastapi import APIRouter, Depends, HTTPException, status
+from sentence_transformers import CrossEncoder
+from torch.nn.functional import softmax
 from tqdm import tqdm
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 from src.api.auth.bearer import auth_secret_key
 from src.api.model.reranker import ListRerankerResponse, RerankerRequest, RerankerResponse
 from src.config.arg import ARGUMENTS
+from src.config.env import BATCH_SIZE, DEVICE
 from src.logger import logger
 
 reranker_router = APIRouter()
 
 
-def _load_reranker_models(
-    reranks: List[str], rerank_seq_lens: List[int]
-) -> Dict[str, Dict[str, AutoModelForSequenceClassification | AutoTokenizer | int]]:
+def _load_reranker_models(reranks: List[str], rerank_max_lengths: List[int]) -> Dict[str, Dict[str, CrossEncoder | int]]:
     name_reranker_map = {}
 
-    if len(reranks) != len(rerank_seq_lens):
+    if len(reranks) != len(rerank_max_lengths):
         logger.error("[Load Rerank Models] Model and sequence length number mismatch")
         exit(-1)
 
-    for model, seq_len in tqdm(zip(reranks, rerank_seq_lens), desc="Load Rerank Models"):
+    for model, max_length in tqdm(zip(reranks, rerank_max_lengths), desc="Load Rerank Models"):
         _model = Path(model)
         if not _model.exists():
             logger.error(f"[Load Rerank Models] Model not found: {model}")
             exit(-1)
 
-        logger.debug(f"[Load Reranker Models] loading model: `{_model.name}` with sequence length: {seq_len} from path: {model}")
+        logger.debug(f"[Load Reranker Models] loading model: `{_model.name}` with max sequence length: {max_length} from path: {model}")
         name_reranker_map[_model.name] = {}
-        name_reranker_map[_model.name]["model"] = AutoModelForSequenceClassification.from_pretrained(model)
-        name_reranker_map[_model.name]["tokenizer"] = AutoTokenizer.from_pretrained(model)
-        name_reranker_map[_model.name]["max_length"] = seq_len
+        name_reranker_map[_model.name]["model"] = CrossEncoder(model, max_length=max_length, device=DEVICE)
+        name_reranker_map[_model.name]["max_length"] = max_length
 
     return name_reranker_map
 
@@ -58,25 +55,23 @@ async def rerank(request: RerankerRequest) -> RerankerResponse:
 
     try:
         model = NAME_RERANKER_MAP[request.model]["model"]
-        tokenizer = NAME_RERANKER_MAP[request.model]["tokenizer"]
-        max_length = NAME_RERANKER_MAP[request.model]["max_length"]
     except KeyError:
         logger.error(f"[Embedding] Invalid model name: {request.model}")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Invalid model name: {request.model}",
         )
-    with torch.no_grad():
-        inputs = tokenizer(pairs, padding=True, truncation=True, max_length=max_length, return_tensors="pt")
-        scores = (
-            model(**inputs, return_dict=True)
-            .logits.view(
-                -1,
-            )
-            .float()
-        )
-        if request.normalize:
-            scores = F.softmax(scores, dim=0)
+    scores = model.predict(
+        pairs,
+        batch_size=BATCH_SIZE,
+        show_progress_bar=True,
+        activation_fct=lambda x: x,  # NOTE sentence_transformers CrossEncoder will use sigmoid to normalize the score. Disable it here.
+        convert_to_tensor=True,
+        convert_to_numpy=False,
+    )
+
+    if request.normalize:
+        scores = softmax(scores, dim=0)
 
     scores = scores.numpy()
     scores, ranks = scores.tolist(), (-scores).argsort().argsort().tolist()
