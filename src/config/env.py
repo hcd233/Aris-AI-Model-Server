f09843b5c0a848986ae27c106b779f9e31030bdc
centonhuang <centonhuang@tencent.com>
2024-04-06 16:28:43 +0800

    ✨ feat(mas): update batch size env

--- src/config/env.py
+++ src/config/env.py
@@ -1,6 +1,7 @@
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 1))
 DEVICE = os.environ.get("DEVICE")
 
 PORT = int(os.environ.get("PORT", "8080"))
