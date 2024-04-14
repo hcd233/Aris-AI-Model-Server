dac24d1ff7e2bf673c98069c8dea89637c60dbfd
centonhuang <centonhuang@tencent.com>
2024-04-14 22:22:08 +0800

    🦄 refactor(mas): add api logs

--- src/config/gbl.py
+++ src/config/gbl.py
@@ -1,4 +1,5 @@
 INFO_LOG = "model-api-server-info.log"
+SUCCESS_LOG = "model-api-server-success.log"
 ERROR_LOG = "model-api-server-error.log"
 
 LOG_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level}</level> | <cyan>[{function}]</cyan>: <level>{message}</level>"
--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 from sys import stdout
 
-from loguru._logger import Logger
 from loguru import logger as _logger
+from loguru._logger import Logger
 
 from src.config.env import LOGGER_LEVEL, LOGGER_ROOT
-
-from src.config.gbl import ERROR_LOG, INFO_LOG, LOG_FORMAT
+from src.config.gbl import ERROR_LOG, INFO_LOG, LOG_FORMAT, SUCCESS_LOG
 
 
 def init_logger() -> Logger:
@@ -20,6 +19,7 @@ def init_logger() -> Logger:
     _logger.remove()  # remove origin handler
     _logger.add(stdout, colorize=True, enqueue=True, level=LOGGER_LEVEL, format=LOG_FORMAT)
     _logger.add(log_root.joinpath(INFO_LOG), encoding="utf-8", rotation="100MB", enqueue=True, level="INFO", format=LOG_FORMAT)
+    _logger.add(log_root.joinpath(SUCCESS_LOG), encoding="utf-8", rotation="100MB", enqueue=True, level="SUCCESS", format=LOG_FORMAT)
     _logger.add(log_root.joinpath(ERROR_LOG), encoding="utf-8", rotation="100MB", enqueue=True, level="ERROR", format=LOG_FORMAT)
 
     _logger.info("Init logger successfully")
