f198b766d8659eb94e5d311097331e862bf6d2ac
centonhuang <centonhuang@tencent.com>
2024-04-26 23:00:48 +0800

    ✨ feat(mas): use yaml to store model config

--- src/config/arg.py
+++ src/config/arg.py
@@ -1,33 +1,83 @@
 from argparse import ArgumentParser
+from os import PathLike
+from pathlib import Path
 from typing import Any, Dict
+
+import yaml
+from pydantic import BaseModel
 
 from src.logger import logger
 
 
 def parse_args() -> Dict[str, Any]:
     parser = ArgumentParser()
-    parser.add_argument("--embed", "-e", type=str, help="The embedding path to load", action="append", default=[])
-    parser.add_argument("--embed_seq_len", "-esl", type=int, help="The embedding max sequence length", action="append", default=[])
-    parser.add_argument("--rerank", "-r", type=str, help="The reranker path to load", action="append", default=[])
-    parser.add_argument("--rerank_seq_len", "-rsl", type=int, help="The reranker max sequence length", action="append", default=[])
+    parser.add_argument("--config_path", "-cp", type=str, help="Path to model config file, in yaml format", required=True)
 
     parser.add_argument("--use_cache", "-uc", type=bool, help="Use cache", default=False)
 
     args = vars(parser.parse_args())
 
-    if not args["embed"]:
-        logger.warning("[Check Arg] Embedding path is required, specify with -e or --embed")
+    path = Path(args["config_path"])
+    if not path.exists():
+        logger.error(f"Config file {args['config_path']} not found")
+        exit(1)
 
-    if not args["embed_seq_len"]:
-        logger.warning("[Check Arg] Embedding Sequence length is required, specify with -esl or --embed_seq_len")
-
-    if not args["rerank"]:
-        logger.warning("[Check Arg] Reranker path is required, specify with -r or --rerank")
-
-    if not args["rerank_seq_len"]:
-        logger.warning("[Check Arg] Reranker Sequence length is required, specify with -rsl or --rerank_seq_len")
+    if path.suffix not in [".yaml", ".yml"]:
+        logger.error(f"Config file {args['config_path']} must be in yaml format")
+        exit(1)
 
     return args
 
 
+class LLMConfig(BaseModel):
+    pass
+
+
+class EmbeddingConfig(BaseModel):
+    alias: str
+    path: str
+    batch_size: int
+    max_seq_len: int
+
+
+class RerankerConfig(BaseModel):
+    alias: str
+    path: str
+    batch_size: int
+    max_seq_len: int
+
+
+class ModelConfig(BaseModel):
+    llm_configs: Dict[str, LLMConfig]
+    embedding_configs: Dict[str, EmbeddingConfig]
+    reranker_configs: Dict[str, RerankerConfig]
+
+    @classmethod
+    def from_yaml(cls, path: PathLike) -> "ModelConfig":
+        with open(path, "r") as fp:
+            config = yaml.safe_load(fp)
+
+        llm_configs, embedding_configs, reranker_configs = {}, {}, {}
+
+        for alias, kwargs in config.get("embedding", {}).items():
+            if alias in embedding_configs:
+                logger.error(f"Duplicate embedding alias: {alias}")
+                exit(1)
+            embedding_configs[alias] = EmbeddingConfig(alias=alias, **kwargs)
+
+        for alias, kwargs in config.get("reranker", {}).items():
+            if alias in reranker_configs:
+                logger.error(f"Duplicate reranker alias: {alias}")
+                exit(1)
+            reranker_configs[alias] = RerankerConfig(alias=alias, **kwargs)
+
+        return cls(
+            llm_configs=llm_configs,
+            embedding_configs=embedding_configs,
+            reranker_configs=reranker_configs,
+        )
+
+
 ARGUMENTS = parse_args()
+
+MODEL_CONFIG = ModelConfig.from_yaml(ARGUMENTS["config_path"])
