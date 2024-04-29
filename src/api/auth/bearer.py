3348c2ecc0ebb9cf010a885492d8be9bc08791cc
centonhuang <centonhuang@tencent.com>
2024-04-29 18:05:19 +0800

    🦄 refactor(lgs): refactor auth status code

--- src/api/auth/bearer.py
+++ src/api/auth/bearer.py
@@ -9,8 +9,14 @@ bearer_scheme = HTTPBearer()
 
 
 async def auth_secret_key(bearer_auth: Optional[str] = Depends(bearer_scheme)) -> None:
-    if not bearer_auth or bearer_auth.credentials != SECRET_KEY:
+    if not bearer_auth:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Not authenticated",
+        )
+
+    if bearer_auth.credentials != SECRET_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid authentication credentials",
         )
