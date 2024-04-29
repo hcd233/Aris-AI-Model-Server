2b04d83295e696e07339e750a63dede888d51c0a
centonhuang <centonhuang@tencent.com>
2024-04-29 18:11:02 +0800

    ✨ feat(mas): support vllm openai compatible chat api

--- src/api/router/v1/__init__.py
+++ src/api/router/v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .embedding import embedding_router
 from .reranker import reranker_router
+from .chat_cmpl import chat_completion_router
 
 v1_router = APIRouter(prefix="/v1", tags=["v1"])
 v1_router.include_router(embedding_router)
 v1_router.include_router(reranker_router)
+v1_router.include_router(chat_completion_router)
 
 __all__ = ["v1_router"]
